4e1aaf1f6e693c0acc11d8b1b6c2c3cf03b3d333
Bruno Henrique <brunohenrique.ramos@live.com>
2021-07-19 17:00:24 -0300

    style(global): added colors variables in global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -14,11 +14,24 @@ export default createGlobalStyle`
 
   :root {
     // Colors
-    --color-primary: #722456;
-    --color-gray-500: #d0d0ce;
+    --color-primary: #2891F9;
+    --color-secundary: #153A71;
+    --color-gray-500: #C2C2C2;
+    --color-gray-700: #666666;
+    --color-gray-800: #313A42;
     --color-black: #000000;
     --color-white: #FFFFFF;
-    --color-blue: #00ACED;
+
+    // Footer Gradient Colors
+    --color-gradient-1: #E5F0FF;
+    --color-gradient-2: #DEE8F7;
+    --color-gradient-3: #D7E0ED;
+    --color-gradient-4: #CCD5E1;
+
+    // Alert Colors
+    --color-danger: #EA5050;
+    --color-warning: #D89F49;
+    --color-success: #47D66F;
 
     // Text Colors
     --color-text-white: #FFFFFF;
