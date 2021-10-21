92bad179e06e08efd3949722f95c879a444b4c0c
Bruno Henrique <brunohenrique.ramos@live.com>
2021-10-21 20:50:04 -0300

    feat: add superswap fav and title

--- src/pages/superswap.tsx
+++ src/pages/superswap.tsx
@@ -1,6 +1,22 @@
 // Components
 import SuperSwap from '@/components/templates/SuperSwap'
+import Head from 'next/head'
 
 export default function Super() {
-  return <SuperSwap />
+  return (
+    <>
+      <Head>
+        <title>SuperSwap</title>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/faviconsuper.ico"
+        />
+        <meta charSet="utf-8" />
+        <meta name="description" content="SwapMatic" />
+      </Head>
+      <SuperSwap />
+    </>
+  )
 }
